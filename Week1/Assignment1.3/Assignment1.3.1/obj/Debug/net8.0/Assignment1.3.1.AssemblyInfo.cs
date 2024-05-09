bf579ceaf3efe2a15ff7956f4ad015750ac03e7e
Jason Wheeler <wheeler.jason@live.com>
2024-05-09 15:34:11 +0200

    add assignment 1.3.2, fix bug in 1.3.1

--- Week1/Assignment1.3/Assignment1.3.1/obj/Debug/net8.0/Assignment1.3.1.AssemblyInfo.cs
+++ Week1/Assignment1.3/Assignment1.3.1/obj/Debug/net8.0/Assignment1.3.1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1.3.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1261c26ad18eeeed2cd23acbb1b2e6dfbf7f57c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1.3.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1.3.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Week1/Assignment1.3/Assignment1.3.2/Program.cs
+++ Week1/Assignment1.3/Assignment1.3.2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Array Reversal - Assignment 1.3.2\n");
+            Console.WriteLine("How many numbers would you like to input into the array?\n");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // Read elements into the original array
+            int[] originalArray = new int[arraySize];
+            Console.WriteLine($"Input {arraySize} numbers into the array:\n");
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"element - {i} : ");
+                originalArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            // Display array contents
+            Console.WriteLine("\nThe values stored in the array are: ");
+            foreach (int i in originalArray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            // Reverse the array - not actually necessary based on problem statement
+           
+
+            // Display the array contents in reverse
+            Console.WriteLine("\nThe values stored in the array in reverse are:");
+            for (int i = arraySize-1; i >= 0; i--)
+            {
+                Console.Write(originalArray[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
