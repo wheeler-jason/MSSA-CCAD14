87b9b0424a3c3f5337abd247abb4c05102e9b675
Jason Wheeler <wheeler.jason@live.com>
2024-05-09 20:32:32 +0200

    modify Assignment1.3.2 to reverse the array rather than printing it in reverse

--- Week1/Assignment1.3/Assignment1.3.2/Program.cs
+++ Week1/Assignment1.3/Assignment1.3.2/Program.cs
@@ -9,31 +9,37 @@
             int arraySize = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
-            // Read elements into the original array
-            int[] originalArray = new int[arraySize];
+            // Read elements into the array
+            int[] numArray = new int[arraySize];
             Console.WriteLine($"Input {arraySize} numbers into the array:\n");
             for (int i = 0; i < arraySize; i++)
             {
                 Console.Write($"element - {i} : ");
-                originalArray[i] = Convert.ToInt32(Console.ReadLine());
+                numArray[i] = Convert.ToInt32(Console.ReadLine());
             }
             
-            // Display array contents
+            // Display array contents before reversing it
             Console.WriteLine("\nThe values stored in the array are: ");
-            foreach (int i in originalArray)
+            foreach (int i in numArray)
             {
                 Console.Write(i + " ");
             }
             Console.WriteLine();
 
-            // Reverse the array - not actually necessary based on problem statement
-           
-
-            // Display the array contents in reverse
-            Console.WriteLine("\nThe values stored in the array in reverse are:");
-            for (int i = arraySize-1; i >= 0; i--)
+            // Reverse the array
+            int temp;
+            for (int i = 0;i < arraySize/2; i++)
             {
-                Console.Write(originalArray[i] + " ");
+                temp = numArray[i];
+                numArray[i] = numArray[arraySize - i - 1];
+                numArray[arraySize - i - 1] = temp;
+            }
+
+            // Display the array contents after reversing it
+            Console.WriteLine("\nThe values stored in the reversed array are: ");
+            foreach (int i in numArray)
+            {
+                Console.Write(i + " ");
             }
             Console.WriteLine();
         }
