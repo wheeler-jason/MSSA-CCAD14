8251e95f0b6d7864f802ec8e23c6712b73f5ee34
Jason Wheeler <wheeler.jason@live.com>
2024-05-13 21:46:58 +0200

    add Assignment2.1.3

---
+++ Week2/Assignment2.1/Assignment2.1.3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace Assignment2._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment 2.1.3");
+        }
+    }
+
+    public static class Maths
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2, decimal num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public static float Multiply(float num1, float num2) 
+        { 
+            return num1 * num2;
+        }
+
+        public static float Multiply(float num1, float num2, float num3)
+        {
+            return num1 * num2 * num3;
+        }
+    }
+}
