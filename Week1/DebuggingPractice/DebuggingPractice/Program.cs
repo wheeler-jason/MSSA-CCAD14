9076798838d1037f1b59e92f70dcdc56c1caa4e7
Jason Wheeler <wheeler.jason@live.com>
2024-05-13 20:42:23 +0200

    add Assignment2.1.1

---
+++ Week1/DebuggingPractice/DebuggingPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DebuggingPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment 1.3.1 - Debugging Practice");
+
+            // Generate a mathematical error and utilize exception handling
+            try
+            {
+
+            } 
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
---
+++ Week2/Assignment2.1/Assignment2.1.1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Assignment2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment 2.1.1");
+            Student student1 = new Student();
+            student1.StudentFName = "John";
+            student1.StudentLName = "Doe";
+            student1.StudentId = 123;
+            student1.StudentGrade = 'A';
+            Console.WriteLine($"First Name: {student1.StudentFName}");
+            Console.WriteLine($"Last Name: {student1.StudentLName}");
+            Console.WriteLine($"Student ID: {student1.StudentId}");
+            Console.WriteLine($"Grade: {student1.StudentGrade}");
+
+        }
+    }
+
+    class Student 
+    {
+        private int _studentId;
+        
+        public int StudentId
+        {
+            get { return _studentId; }
+            set { _studentId = value; }
+        }
+
+        private string _studentFName;
+
+        public string StudentFName
+        {
+            get { return _studentFName; }
+            set { _studentFName = value; }
+        }
+
+        private string _studentLName;
+
+        public string StudentLName
+        {
+            get { return _studentLName; }
+            set { _studentLName = value; }
+        }
+
+        private char _studentGrade;
+
+        public char StudentGrade
+        {
+            get { return _studentGrade; }
+            set { _studentGrade = value; }
+        }
+    }
+}
