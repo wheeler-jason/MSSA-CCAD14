83bb3eda447e7129d4bf33195a97b1f314c636fd
Jason Wheeler <wheeler.jason@live.com>
2024-06-05 20:42:42 +0200

    add Assignment5.3

---
+++ Week5/Assignment5.3/Assignment5.3.1/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Assignment5._3._1
+{
+    /*
+     * You have a long flowerbed in which some of the plots are planted, and some are not. 
+     * However, flowers cannot be planted in adjacent plots.
+     * Given an integer array flowerbed containing 0's and 1's, 
+     * where 0 means empty and 1 means not empty, and an integer n, 
+     * return true if n new flowers can be planted in the flowerbed without 
+     * violating the no-adjacent-flowers rule and false otherwise.
+     * Example 1:
+     * Input: flowerbed = [1,0,0,0,1], n = 1
+     * Output: true
+     * Example 2:
+     * Input: flowerbed = [1,0,0,0,1], n = 2
+     * Output: false
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testFlowerBed = [0, 0, 1, 0, 0];
+            Console.WriteLine(PlantNonAdjacentFlowers(testFlowerBed, 1));
+        }
+
+        public static bool PlantNonAdjacentFlowers(int[] flowerbed, int n)
+        {
+            int i = 0;
+            int flowersPlantedCounter = 0;
+
+            // if there is only a single zero in the array
+            if (flowerbed.Length == 1 && n == 1 && flowerbed[0] == 0 )
+            {
+                return true;
+            }
+
+            while (i < flowerbed.Length)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    // if we're at the start of the array
+                    if (i == 0)
+                    {
+                        // if the next element is also a 0, then plant the flower
+                        if (flowerbed[i + 1] == 0)
+                        {
+                            flowerbed[i] = 1;
+                            flowersPlantedCounter++;
+                            i++;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    // if we're at the end of the array
+                    else if (i == flowerbed.Length-1)
+                    {
+                        // if the previous element is also a 0, then plant the flower
+                        if (flowerbed[i-1] == 0)
+                        {
+                            flowerbed[i] = 1;
+                            flowersPlantedCounter++;
+                            i++;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    // if we're in the middle of the array
+                    else if (i > 0 && i < flowerbed.Length-1)
+                    {
+                        // if we have zeroes on both sides
+                        if (flowerbed[i-1] == 0 && flowerbed[i+1] == 0)
+                        {
+                            flowerbed[i] = 1;
+                            flowersPlantedCounter++;
+                            i++;
+                        } 
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return flowersPlantedCounter >= n;
+        }
+    }
+}
---
+++ Week5/Assignment5.3/Assignment5.3.2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Assignment5._3._2
+{
+    /*
+     * You are climbing a staircase. It takes n steps to reach the top.
+     * Each time you can either climb 1 or 2 steps. 
+     * In how many distinct ways can you climb to the top?
+     * Example 1:
+     * Input: n = 2
+     * Output: 2
+     * Explanation: There are two ways to climb to the top.
+     * 1. 1 step + 1 step
+     * 2. 2 steps
+     * Example 2:
+     * Input: n = 3
+     * Output: 3
+     * Explanation: There are three ways to climb to the top.
+     * 1. 1 step + 1 step + 1 step
+     * 2. 1 step + 2 steps
+     * 3. 2 steps + 1 step
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of steps:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Number of distinct ways to climb to the top: {ClimbStairs(n)}");
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            // Initialize the base cases
+            int[] result = new int[n+1];
+            result[0] = 1;
+            result[1] = 1;
+
+            // Fill the result array
+            for (int i = 2; i <= n; i++)
+            {
+                result[i] = result[i-1] + result[i-2];
+            }
+
+            return result[n];
+        }
+    }
+}
