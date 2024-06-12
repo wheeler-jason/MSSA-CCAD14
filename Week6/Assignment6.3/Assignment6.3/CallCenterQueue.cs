55098223241dcaea1f994bc243b298c8644b90d6
Jason Wheeler <wheeler.jason@live.com>
2024-06-12 15:18:01 +0200

    add Assignment6.3

---
+++ Week6/Assignment6.3/Assignment6.3/CallCenterQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6._3
+{
+    internal class CallCenterQueue
+    {
+        private LinkedList<string> callQueue = new LinkedList<string>();
+
+        public void Enqueue(string call)
+        {
+            callQueue.AddLast(call);
+            Console.WriteLine($"{call} has been added to the queue");
+        }
+        
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty");
+                return;
+            }
+            string call = callQueue.First.Value;
+            callQueue.RemoveFirst();
+            Console.WriteLine($"{call} has been removed from the queue");
+        }
+
+        public void Clear() {
+            callQueue.Clear();
+            Console.WriteLine("The queue has been cleared");
+        }
+
+        public void PrintQueue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty");
+                return;
+            }
+            Console.WriteLine("The calls in the queue are:");
+            foreach (string call in callQueue)
+            {
+                Console.WriteLine(call);
+            }
+        }
+
+        public void Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty");
+                return;
+            }
+            Console.WriteLine($"The next call in the queue is {callQueue.First.Value}");
+        }
+
+        public bool IsEmpty()
+        {
+            return callQueue.Count == 0;
+        }
+    }
+}
---
+++ Week6/Assignment6.3/Assignment6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment6._3
+{
+    // Implement a queue using LinkedList to
+    // model a call center call queue
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CallCenterQueue callCenterQueue = new CallCenterQueue();
+            callCenterQueue.Enqueue("Call 1");
+            callCenterQueue.Enqueue("Call 2");
+            callCenterQueue.Enqueue("Call 3");
+            callCenterQueue.Enqueue("Call 4");
+            callCenterQueue.Enqueue("Call 5");
+            callCenterQueue.Enqueue("Call 6");
+            callCenterQueue.Enqueue("Call 7");
+            callCenterQueue.Enqueue("Call 8");
+            callCenterQueue.Enqueue("Call 9");
+            callCenterQueue.Enqueue("Call 10");
+            callCenterQueue.PrintQueue();
+            callCenterQueue.Dequeue();
+            callCenterQueue.Dequeue();
+            callCenterQueue.Dequeue();
+            callCenterQueue.PrintQueue();
+            callCenterQueue.Clear();
+            callCenterQueue.PrintQueue();
+        }
+    }
+}
