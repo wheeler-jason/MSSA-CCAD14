e290fb46ef18a606d460e1457ab1655ba2313634
Jason Wheeler <wheeler.jason@live.com>
2024-06-17 16:16:57 +0200

    add Assignment7.1

---
+++ Week7/Assignment7.1/Assignment7.1.1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Assignment7._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] examScores = GenerateRandomArray(100);
+            Console.WriteLine("Unsorted Array:");
+            PrintArray(examScores);
+            SelectionSort(examScores);
+            Console.WriteLine("Sorted Array:");
+            PrintArray(examScores);
+
+        }
+
+        // Generates an array of random integers of size n 
+        // with numbers in the range of [0-99]
+        public static int[] GenerateRandomArray(int n)
+        {
+            Random random = new Random();
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+            return array;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    int temp = array[minIndex];
+                    array[minIndex] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ Week7/Assignment7.1/Assignment7.1.2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment7._1._2
+{
+    /* You are given two strings word1 and word2. 
+     * Merge the strings by adding letters in alternating order, 
+     * starting with word1. If a string is longer than the other, 
+     * append the additional letters onto the end of the merged string.
+     * Return the merged string.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string MergeAlternately(string word1, string word2)
+        {
+            string result = "";
+            int i = 0;
+            while (i < word1.Length && i < word2.Length)
+            {
+                result += word1[i];
+                result += word2[i];
+                i++;
+            }
+
+            // only one of these while loops will execute
+            // depending on which string is longer
+            while (i < word1.Length)
+            {
+                result += word1[i];
+                i++;
+            }
+            while (i < word2.Length)
+            {
+                result += word2[i];
+                i++;
+            }
+            return result;
+        }
+    }
+}
