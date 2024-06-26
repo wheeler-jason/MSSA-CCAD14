2dcfed0823da51527fdea4e6ffc597a97fb671d2
Jason Wheeler <wheeler.jason@live.com>
2024-06-26 22:19:02 +0200

    add assignments 8.1 & 8.2

---
+++ Week8/Assignment8.1/Assignment8.1/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Assignment8._1
+{
+    internal class Program
+    {
+        static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[low]; 
+            int i = low; 
+
+            for (int j = low + 1; j <= high; j++)
+            {
+                if (array[j] < pivot) 
+                {
+                    i++; 
+                    Swap(array, i, j); 
+                }
+            }
+
+            // Swap the pivot element into the correct place
+            Swap(array, low, i);
+
+            // Return the index of the pivot element
+            return i;
+        }
+
+        static void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+
+                // Print partitions for demo purposes
+                Console.Write($"Pivot: {array[pivotIndex]}, Partitions: ");
+                PrintPartition(array, low, pivotIndex - 1);
+                Console.Write(" | ");
+                PrintPartition(array, pivotIndex + 1, high);
+                Console.WriteLine();
+
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] data = { 6, 10, 13, 5, 8, 3, 2, 11 };
+            int size = data.Length;
+
+            Console.WriteLine("Unsorted Array:");
+            Console.WriteLine(string.Join(" ", data));
+            Console.WriteLine();
+
+            Console.WriteLine("Visualizing the Partition() method:\n");
+            QuickSort(data, 0, size - 1);
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted Array in Ascending Order:");
+            Console.WriteLine(string.Join(" ", data));
+        }
+
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        public static void PrintPartition(int[] array, int start, int end)
+        {
+            Console.Write("[");
+            for(int i = start; i <= end; i++)
+            {
+                Console.Write($" {array[i]} ");
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ Week8/Assignment8.2/Assignment8.2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Assignment8._2
+{
+    /*
+     * Given the root of a binary tree, check whether it is
+     * a mirror of itself (i.e., symmetric around its center).
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            return IsMirror(root.left, root.right);
+        }
+
+        public bool IsMirror(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+            {
+                return true;
+            }
+            if (t1 == null || t2 == null)
+            {
+                return false;
+            }
+            if(t1.val != t2.val)
+            {
+                return false;
+            }
+            return IsMirror(t1.left, t2.right) && IsMirror(t1.right, t2.left);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
